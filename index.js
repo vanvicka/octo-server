a0f08db74a8d617c7576d68a25ac31205a9f7edf
VICHA <vanvick24@yahoo.com>
2022-10-04 13:28:21 +0100

    initial webhook commmit

--- index.js
+++ index.js
@@ -1,60 +1,14 @@
-var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server)
 
-io.on('connection', function (client) {
-
-  
-  console.log(client.id, 'joined');
-  client.on('/test', function (msg) {
-      console.log(msg);
-  });
-
-  client.on('typing', function name(data) {
-    console.log(data);
-    io.emit('typing', data)
-  })
-
-  client.on('message', function name(data) {
-    console.log(data);
-    io.sockets.emit('message', data)
-  })
-
-  client.on('request', function request(data) {
-    console.log(data);
-    io.sockets.emit('request', data);
-    
-  })
-
-  client.on('response', function name(data) {
-    console.log(data);
-    console.log(data["id"]);
-    // client.removeListener('request', {});
-
-    client.to(data["id"]).emit('response', data);
-    client.broadcast.emit("response", data["status"]);
-    
-  })
-
-  client.on('location', function name(data) {
-    console.log(data);
-    io.emit('location', data);
-  })
-
-  client.on('connect', function () {
-  })
-
-  client.on('disconnect', function () {
-    console.log('client disconnect...', client.id)
-    // handleDisconnect()
-  })
-
-  client.on('error', function (err) {
-    console.log('received error from client:', client.id)
-    console.log(err)
-  })
-})
+app.post("verify_webhook", function(req, res) {
+  // Retrieve the request's body
+  const event = req.body;
+  console.log(event);
+  // Do something with event
+  res.send(200);
+});
 
 var server_port = process.env.PORT || 8080;
 server.listen(server_port, function (err) {
